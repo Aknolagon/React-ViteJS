39596a24ff1be907beb7af3fbf8d1ab384706f7a
Aknolagon <kalki.prasanna.co@gmail.com>
2023-10-10 21:28:33 +0200

    add mew and next pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,30 @@
+import React, { useState } from 'react';
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-    return (
+  const [pokemon, pokemon1] = useState(pokemonList[0]);
+
+  function nextPokemon() {
+   pokemon1(pokemonList[1]);
+  } 
+
+    return(
       <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
-        <figcaption>Bulbasaur</figcaption>
-        </figure>
-    );
-  }
+        {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>)
+        : (<p>???</p>)}
+        <figcaption>{pokemon.name}</figcaption>
+        <button onClick={nextPokemon}>next pokemon</button>
+        </figure>);
+}
   
   export default PokemonCard;
